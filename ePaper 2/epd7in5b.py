9f7c043a327b5e4a4083f972bf95c4c0fdc94818
holmesha <97241809+holmesha@users.noreply.github.com>
2022-11-06 12:55:40 -0800

    Update epd7in5b.py
    Changed code to v2 (from Waveshare)

--- ePaper 2/epd7in5b.py
+++ ePaper 2/epd7in5b.py
@@ -1,204 +1,192 @@
-##
- #  @filename   :   epd7in5.py
- #  @brief      :   Implements for Dual-color e-paper library
- #  @author     :   Yehui from Waveshare
- #
- #  Copyright (C) Waveshare     July 10 2017
- #
- # Permission is hereby granted, free of charge, to any person obtaining a copy
- # of this software and associated documnetation files (the "Software"), to deal
- # in the Software without restriction, including without limitation the rights
- # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- # copies of the Software, and to permit persons to  whom the Software is
- # furished to do so, subject to the following conditions:
- #
- # The above copyright notice and this permission notice shall be included in
- # all copies or substantial portions of the Software.
- #
- # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- # FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- # LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- # THE SOFTWARE.
- #
+# *****************************************************************************
+# * | File        :	  epd7in5b_V2.py
+# * | Author      :   Waveshare team
+# * | Function    :   Electronic paper driver
+# * | Info        :
+# *----------------
+# * | This version:   V4.2
+# * | Date        :   2022-01-08
+# # | Info        :   python demo
+# -----------------------------------------------------------------------------
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documnetation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to  whom the Software is
+# furished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
 
-import epdif
-import Image
-import RPi.GPIO as GPIO
+
+import logging
+from . import epdconfig
 
 # Display resolution
-EPD_WIDTH       = 640
-EPD_HEIGHT      = 384
+EPD_WIDTH       = 800
+EPD_HEIGHT      = 480
 
-# EPD7IN5 commands
-PANEL_SETTING                               = 0x00
-POWER_SETTING                               = 0x01
-POWER_OFF                                   = 0x02
-POWER_OFF_SEQUENCE_SETTING                  = 0x03
-POWER_ON                                    = 0x04
-POWER_ON_MEASURE                            = 0x05
-BOOSTER_SOFT_START                          = 0x06
-DEEP_SLEEP                                  = 0x07
-DATA_START_TRANSMISSION_1                   = 0x10
-DATA_STOP                                   = 0x11
-DISPLAY_REFRESH                             = 0x12
-IMAGE_PROCESS                               = 0x13
-LUT_FOR_VCOM                                = 0x20
-LUT_BLUE                                    = 0x21
-LUT_WHITE                                   = 0x22
-LUT_GRAY_1                                  = 0x23
-LUT_GRAY_2                                  = 0x24
-LUT_RED_0                                   = 0x25
-LUT_RED_1                                   = 0x26
-LUT_RED_2                                   = 0x27
-LUT_RED_3                                   = 0x28
-LUT_XON                                     = 0x29
-PLL_CONTROL                                 = 0x30
-TEMPERATURE_SENSOR_COMMAND                  = 0x40
-TEMPERATURE_CALIBRATION                     = 0x41
-TEMPERATURE_SENSOR_WRITE                    = 0x42
-TEMPERATURE_SENSOR_READ                     = 0x43
-VCOM_AND_DATA_INTERVAL_SETTING              = 0x50
-LOW_POWER_DETECTION                         = 0x51
-TCON_SETTING                                = 0x60
-TCON_RESOLUTION                             = 0x61
-SPI_FLASH_CONTROL                           = 0x65
-REVISION                                    = 0x70
-GET_STATUS                                  = 0x71
-AUTO_MEASUREMENT_VCOM                       = 0x80
-READ_VCOM_VALUE                             = 0x81
-VCM_DC_SETTING                              = 0x82
+logger = logging.getLogger(__name__)
 
 class EPD:
     def __init__(self):
-        self.reset_pin = epdif.RST_PIN
-        self.dc_pin = epdif.DC_PIN
-        self.busy_pin = epdif.BUSY_PIN
+        self.reset_pin = epdconfig.RST_PIN
+        self.dc_pin = epdconfig.DC_PIN
+        self.busy_pin = epdconfig.BUSY_PIN
+        self.cs_pin = epdconfig.CS_PIN
         self.width = EPD_WIDTH
         self.height = EPD_HEIGHT
 
-    def digital_write(self, pin, value):
-        epdif.epd_digital_write(pin, value)
-
-    def digital_read(self, pin):
-        return epdif.epd_digital_read(pin)
-
-    def delay_ms(self, delaytime):
-        epdif.epd_delay_ms(delaytime)
+    # Hardware reset
+    def reset(self):
+        epdconfig.digital_write(self.reset_pin, 1)
+        epdconfig.delay_ms(200) 
+        epdconfig.digital_write(self.reset_pin, 0)
+        epdconfig.delay_ms(4)
+        epdconfig.digital_write(self.reset_pin, 1)
+        epdconfig.delay_ms(200)   
 
     def send_command(self, command):
-        self.digital_write(self.dc_pin, GPIO.LOW)
-        # the parameter type is list but not int
-        # so use [command] instead of command
-        epdif.spi_transfer([command])
+        epdconfig.digital_write(self.dc_pin, 0)
+        epdconfig.digital_write(self.cs_pin, 0)
+        epdconfig.spi_writebyte([command])
+        epdconfig.digital_write(self.cs_pin, 1)
 
     def send_data(self, data):
-        self.digital_write(self.dc_pin, GPIO.HIGH)
-        # the parameter type is list but not int
-        # so use [data] instead of data
-        epdif.spi_transfer([data])
+        epdconfig.digital_write(self.dc_pin, 1)
+        epdconfig.digital_write(self.cs_pin, 0)
+        epdconfig.spi_writebyte([data])
+        epdconfig.digital_write(self.cs_pin, 1)
+    
+    def send_data2(self, data): #faster
+        epdconfig.digital_write(self.dc_pin, 1)
+        epdconfig.digital_write(self.cs_pin, 0)
+        epdconfig.spi_writebyte2(data)
+        epdconfig.digital_write(self.cs_pin, 1)
 
+    def ReadBusy(self):
+        logger.debug("e-Paper busy")
+        self.send_command(0x71)
+        busy = epdconfig.digital_read(self.busy_pin)
+        while(busy == 0):
+            self.send_command(0x71)
+            busy = epdconfig.digital_read(self.busy_pin)
+        epdconfig.delay_ms(200)
+        logger.debug("e-Paper busy release")
+        
     def init(self):
-        if (epdif.epd_init() != 0):
+        if (epdconfig.module_init() != 0):
             return -1
+            
         self.reset()
-        self.send_command(POWER_SETTING)
-        self.send_data(0x37)
-        self.send_data(0x00)
-        self.send_command(PANEL_SETTING)
-        self.send_data(0xCF)
-        self.send_data(0x08)
-        self.send_command(BOOSTER_SOFT_START)
-        self.send_data(0xc7)
-        self.send_data(0xcc)
-        self.send_data(0x28)
-        self.send_command(POWER_ON)
-        self.wait_until_idle()
-        self.send_command(PLL_CONTROL)
-        self.send_data(0x3c)
-        self.send_command(TEMPERATURE_CALIBRATION)
-        self.send_data(0x00)
-        self.send_command(VCOM_AND_DATA_INTERVAL_SETTING)
-        self.send_data(0x77)
-        self.send_command(TCON_SETTING)
-        self.send_data(0x22)
-        self.send_command(TCON_RESOLUTION)
-        self.send_data(0x02)     #source 640
-        self.send_data(0x80)
-        self.send_data(0x01)     #gate 384
-        self.send_data(0x80)
-        self.send_command(VCM_DC_SETTING)
-        self.send_data(0x1E)      #decide by LUT file
-        self.send_command(0xe5)           #FLASH MODE
-        self.send_data(0x03)
+        
+        # self.send_command(0x06)     # btst
+        # self.send_data(0x17)
+        # self.send_data(0x17)
+        # self.send_data(0x38)        # If an exception is displayed, try using 0x38
+        # self.send_data(0x17)
 
-    def wait_until_idle(self):
-        while(self.digital_read(self.busy_pin) == 0):      # 0: busy, 1: idle
-            self.delay_ms(50)
+        self.send_command(0x01);			#POWER SETTING
+        self.send_data(0x07);
+        self.send_data(0x07);    #VGH=20V,VGL=-20V
+        self.send_data(0x3f);		#VDH=15V
+        self.send_data(0x3f);		#VDL=-15V
 
-    def reset(self):
-        self.digital_write(self.reset_pin, GPIO.LOW)         # module reset
-        self.delay_ms(200)
-        self.digital_write(self.reset_pin, GPIO.HIGH)
-        self.delay_ms(200)    
+        self.send_command(0x04); #POWER ON
+        epdconfig.delay_ms(100);
+        self.ReadBusy();
 
-    def get_frame_buffer(self, image):
-        buf = [0xFF] * (self.width * self.height / 8)
-        # Set buffer to value of Python Imaging Library image.
-        # Image must be in mode L.
-        image_grayscale = image.convert('1')
-        imwidth, imheight = image_grayscale.size
-        if imwidth != self.width or imheight != self.height:
-            raise ValueError('Image must be same dimensions as display \
-                ({0}x{1}).' .format(self.width, self.height))
+        self.send_command(0X00);			#PANNEL SETTING
+        self.send_data(0x0F);   #KW-3f   KWR-2F	BWROTP 0f	BWOTP 1f
 
-        pixels = image_grayscale.load()
-        for y in range(self.height):
-            for x in range(self.width):
-                # Set the bits for the column of pixels at the current position.
-                if pixels[x, y] == 0:
-                    buf[(x + y * self.width) / 8] &= ~(0x80 >> (x % 8))
+        self.send_command(0x61);        	#tres
+        self.send_data(0x03);		#source 800
+        self.send_data(0x20);
+        self.send_data(0x01);		#gate 480
+        self.send_data(0xE0);
+
+        self.send_command(0X15);
+        self.send_data(0x00);
+
+        self.send_command(0X50);			#VCOM AND DATA INTERVAL SETTING
+        self.send_data(0x11);
+        self.send_data(0x07);
+
+        self.send_command(0X60);			#TCON SETTING
+        self.send_data(0x22);
+
+        self.send_command(0x65);
+        self.send_data(0x00);
+        self.send_data(0x00);
+        self.send_data(0x00);
+        self.send_data(0x00);
+    
+        return 0
+
+    def getbuffer(self, image):
+        img = image
+        imwidth, imheight = img.size
+        if(imwidth == self.width and imheight == self.height):
+            img = img.convert('1')
+        elif(imwidth == self.height and imheight == self.width):
+            # image has correct dimensions, but needs to be rotated
+            img = img.rotate(90, expand=True).convert('1')
+        else:
+            logger.warning("Wrong image dimensions: must be " + str(self.width) + "x" + str(self.height))
+            # return a blank buffer
+            return [0x00] * (int(self.width/8) * self.height)
+
+        buf = bytearray(img.tobytes('raw'))
+        # The bytes need to be inverted, because in the PIL world 0=black and 1=white, but
+        # in the e-paper world 0=white and 1=black.
+        for i in range(len(buf)):
+            buf[i] ^= 0xFF
         return buf
 
-    def display_frame(self, frame_buffer_black, frame_buffer_red):
-        self.send_command(DATA_START_TRANSMISSION_1)
-        for i in range(0, self.width / 8 * self.height):
-            temp1 = frame_buffer_black[i]
-            temp2 = frame_buffer_red[i]
-            j = 0
-            while (j < 8):
-                if ((temp2 & 0x80) == 0x00):
-                    temp3 = 0x04                #red
-                elif ((temp1 & 0x80) == 0x00):
-                    temp3 = 0x00                #black
-                else:
-                    temp3 = 0x03                #white
-					
-                temp3 = (temp3 << 4) & 0xFF
-                temp1 = (temp1 << 1) & 0xFF
-                temp2 = (temp2 << 1) & 0xFF
-                j += 1
-                if((temp2 & 0x80) == 0x00):
-                    temp3 |= 0x04              #red
-                elif ((temp1 & 0x80) == 0x00):
-                    temp3 |= 0x00              #black
-                else:
-                    temp3 |= 0x03              #white
-                temp1 = (temp1 << 1) & 0xFF
-                temp2 = (temp2 << 1) & 0xFF
-                self.send_data(temp3)
-                j += 1
-        self.send_command(DISPLAY_REFRESH)
-        self.delay_ms(50)
-        self.wait_until_idle()
+    def display(self, imageblack, imagered):
+        self.send_command(0x10)
+        # The black bytes need to be inverted back from what getbuffer did
+        for i in range(len(imageblack)):
+            imageblack[i] ^= 0xFF
+        self.send_data2(imageblack)
+
+        self.send_command(0x13)
+        self.send_data2(imagered)
+        
+        self.send_command(0x12)
+        epdconfig.delay_ms(100)
+        self.ReadBusy()
+        
+    def Clear(self):
+        buf = [0x00] * (int(self.width/8) * self.height)
+        buf2 = [0xff] * (int(self.width/8) * self.height)
+        self.send_command(0x10)
+        self.send_data2(buf2)
+            
+        self.send_command(0x13)
+        self.send_data2(buf)
+                
+        self.send_command(0x12)
+        epdconfig.delay_ms(100)
+        self.ReadBusy()
 
     def sleep(self):
-        self.send_command(POWER_OFF)
-        self.wait_until_idle()
-        self.send_command(DEEP_SLEEP)
-        self.send_data(0xa5)
-
+        self.send_command(0x02) # POWER_OFF
+        self.ReadBusy()
+        
+        self.send_command(0x07) # DEEP_SLEEP
+        self.send_data(0XA5)
+        
+        epdconfig.delay_ms(2000)
+        epdconfig.module_exit()
 ### END OF FILE ###
 
